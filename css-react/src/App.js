c340e3985807964021e24fa7224df14a347185a0
Yuri <yurifdev@gmail.com>
2022-10-04 19:12:58 -0300

    Example to put class in components

--- css-react/src/App.js
+++ css-react/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponant from './components/MyComponant';
+
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
-      
+      {/* CSS de componente */}
+      <MyComponant />
+      <p>Parágrafo do App.js</p>
     </div>
   );
 }
---
+++ css-react/src/components/MyComponant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./MyComponant.css"
+
+const MyComponant = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <p>Este é o parágrafo do componente</p>
+        <p className="my-comp-paragraph">Maneira certa de fazer o parágrafco. Usando classes próprias</p>
+    </div>
+  )
+}
+
+export default MyComponant
