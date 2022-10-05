6ac5a25c0ecf2ff91ece547073b6c0da83b42f97
Yuri <yurifdev@gmail.com>
2022-10-05 16:33:32 -0300

    Dinamic CSS inline

--- css-react/src/App.js
+++ css-react/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyComponant from './components/MyComponant';
+import { useState } from 'react';
 
 
 function App() {
+  const n = 15;
+  const [name] = useState("Yuri")
+
   return (
     <div className="App">
       {/* CSS global */}
@@ -14,6 +18,11 @@ function App() {
       {/* Inline CSS */}
       {/* A primeira chave é para inserir algo dinâmico, a segunda, um objeto */}
       <p style={{color: "blue", padding: "25px", borderTop: "6px solid red"}}>Esse elemento foi estilizado na própia tag. Sendo não recomendado</p>
+      {/* CSS Inline dinâmico */}
+      <h1 style={n < 10 ? ({color: "purple"}) : ({color: "green"})}>CSS dinâmico</h1>
+      <h1 style={n > 10 ? ({color: "purple"}) : ({color: "green"})}>CSS dinâmico</h1>
+      {name === "Yuri" && <h1 style={name === "Yuri" ? ({color: "green", backgroundColor: "yellow"}) : null}>Teste de nome: De fato, o nome é "{name}"</h1>}
+      
     </div>
   );
 }
