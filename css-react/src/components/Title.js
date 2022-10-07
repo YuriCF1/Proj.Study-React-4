5af378fe6adbc092cbb197d1f23a77707cbad568
Yuri <yurifdev@gmail.com>
2022-10-06 23:24:57 -0300

    Refactoring

--- css-react/src/components/Title.js
+++ css-react/src/components/Title.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import styles from './Title.module.css' 
+import React from "react";
+import styles from "./Title.module.css";
 
 const Title = () => {
   return (
     <div>
-        {/* <h1 className={styles['my-titles']}>Título exemplar</h1> || Se eu fosse usar com traços, seria assim. Mas não é recomendado */}
-        <h1 className={styles.my_title}>Título exemplar</h1>
+      {/* <h1 className={styles['my-titles']}>Título exemplar</h1> || Se eu fosse usar com traços, seria assim. Mas não é recomendado */}
+      <h1 className={styles.my_title}>Título exemplar</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
